0e5f79c678d2b6cd91770dc5458275bcfbe41d36
Jacob Pawek <32315597+Dragongoat@users.noreply.github.com>
2020-03-11 11:29:50 -0700

    Introduced smelly code

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void printWord(string word);
+
 int main()
 {
-    cout<<"Hi, please enter two numbers: ";
+	printWord("Hi, ");
+	printWord("please ");
+	printWord("enter ");
+	printWord("two ");
+	printWord("numbers: ");
 
 		int first = 0;
 		int second = 0;
@@ -14,4 +20,8 @@ int main()
 		cin >> first >> second;
 
     return 0;
+}
+
+void printWord(string word) {
+	cout << word;	
 }
